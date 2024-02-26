55702578fa231a0aec0baf11a920d29bf1ddb5c4
Johan Boer <johan.boer@vng.nl>
2024-02-26 12:12:16 +0100

    w

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/JohanBoer/graphql-golang/internal/pkg/db/mysql/database"
+	"github.com/JohanBoer/graphql-golang/internal/pkg/db/database"
 	"github.com/go-chi/chi"
 )
 
